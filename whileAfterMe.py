809ce75f8d6fdce398dc1a234694c2fbf032f612
amanb <amanbijalwan@gmail.com>
2022-09-06 02:14:45 +0530

    updating while_after_me program.

--- whileAfterMe.py
+++ whileAfterMe.py
@@ -1,6 +1,9 @@
 
-prompt = "\nTell me something and I will repeat it!"
-prompt += "\nEnter 'quit' to exit the program\n"
+print("This will repeat whatever you type.\n")
+print("Type 'quit' to exit\n")
+#prompt = "\nTell me something and I will repeat it!"
+#prompt += "\nEnter 'quit' to exit the program\n"
+prompt = ""
 msg = ""
 
 while msg != 'quit':
